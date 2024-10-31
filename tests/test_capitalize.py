42520cfdcf740d93e8bfeffeb324027b5dd7acac
Marvv1ne <gorn89@inbox.ru>
2024-10-31 22:27:00 +0400

    instead of if inserted assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,8 +1,7 @@
 from capitalize import capitalize
 
 
-#для запуска тестов необходимо прописать в командной строке
-# PYTHONPATH=src python tests/test_capitalize.py
+
 
 if capitalize('hello') != 'Hello':
     raise Exception('Функция работает неверно!')
